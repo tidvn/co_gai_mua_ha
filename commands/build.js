a4f6f591476c8b48e6fce135fb1e52e6c2c32e43
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-04 00:17:22 +0700

    Update build.js

--- commands/build.js
+++ commands/build.js
@@ -5,7 +5,7 @@ function embedsfc(mess) {
 	
 	let tid = new MessageEmbed()
 	
-	.setColor(data[mess].Color)
+	.setColor('RANDOM')
 	.setTitle(data[mess].Title)
 	.setThumbnail(data[mess].Thumbnail)
 	.setImage(data[mess].Image)
@@ -13,6 +13,16 @@ function embedsfc(mess) {
 
 	return { embeds: [tid] };   
   }
+  const help = {
+	color: 'RANDOM',
+	title: 'Đã xảy ra lỗi :( ',
+	
+	
+	description: 'sử dụng lệnh \"\/build \+ tên nhân vật\"\n lưu ý : tên nhân vật phải viết Hoa ký tự đầu tiên.\n 1 số nhân vật có tên 2 chữ như Kamisato Ayaka , Kaedehara Kazuha thì chỉ sử dụng tên (Ayaka, Kazuha) .\n 1 số nhân vậT đặc biệt: Childe = Tartaglia , raiden shogun = Baal ',
+	
+	
+	
+};
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('build')
@@ -20,7 +30,13 @@ module.exports = {
 		.addStringOption(option => option.setName('input').setDescription('nhập tên nhân vật:')),
 	async execute(interaction) {
 		const value = interaction.options.getString('input');
+		
+			
+			
 		if (data[value]) return interaction.reply(embedsfc(value));
-		return interaction.reply('không tồn tại');
+		
+		return interaction.reply({ embeds: [help] })
+		
+		
 	},
 };
