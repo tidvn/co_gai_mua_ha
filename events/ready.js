e9ea2a426ef62ce58337385ffafaf472233ec102
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-08 16:57:09 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
-const antiAd = require('./module_ready_events/anti-ad.js')
+
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		antiAd(client);
+		
 	},
 };
