01b3648473816e002dcbd2767084989bafc6110a
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-08 14:08:33 +0700

    Update build.js

--- commands/build.js
+++ commands/build.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const {MessageEmbed} = require('discord.js');
 const data = require('../assets/buildchar.json');
 const data1 = ` \/build Ganyu \n \/build Sara \n \/build Baal \n \/build Sayu \n \/build Yoimiya \n \/build Ayaka \n \/build Jean \n \/build Jean \n \/build Venti \n \/build Xiao \n \/build Chongyun \n \/build Diona \n \/build Keaya \n \/build Qiqi \n \/build Beidou \n \/build Rosaria \n \/build Mona \n \/build Eula \n \/build Yanfei\n `
-const data2 =` \/build Fischl\n \/build keqing\n \/build Albedo\n \/build Ningguang\n \/build Noelle\n \/build Zhongli\n \/build Babara\n \/build Tartaglia\n \/build Xingqiu\n \/build Bennett\n \/build Diluc\n \/build Klee\n \/build Xiangling\n \/build Kazuha\n \/build Razor\n \/build Amber\n \/build Aloy\n \/build Kokomi\n \/build Hutao\n `
+const data2 =` \/build Fischl\n \/build keqing\n \/build Albedo\n \/build Ningguang\n \/build Noelle\n \/build Zhongli\n \/build Babara\n \/build Childe\n \/build Xingqiu\n \/build Bennett\n \/build Diluc\n \/build Klee\n \/build Xiangling\n \/build Kazuha\n \/build Razor\n \/build Amber\n \/build Aloy\n \/build Kokomi\n \/build Hutao\n `
 function embedsfc(mess) {
 	
 	let tid = new MessageEmbed()
@@ -18,7 +18,7 @@ function embedsfc(mess) {
   const help = {
 	color: 'RANDOM',
 	title: 'Đã xảy ra lỗi :( ',	
-	description: 'sử dụng lệnh \"\/build \+ tên nhân vật\"\n lưu ý : tên nhân vật phải viết Hoa ký tự đầu tiên.\n 1 số nhân vật có tên 2 chữ như Kamisato Ayaka , Kaedehara Kazuha thì chỉ sử dụng tên (Ayaka, Kazuha) .\n 1 số nhân vậT đặc biệt: Childe = Tartaglia , raiden shogun = Baal ',
+	description: 'sử dụng lệnh \"\/build \+ tên nhân vật\"\n lưu ý : tên nhân vật phải viết Hoa ký tự đầu tiên.\n 1 số nhân vật có tên 2 chữ như Kamisato Ayaka , Kaedehara Kazuha thì chỉ sử dụng tên (Ayaka, Kazuha) .\n 1 số nhân vậT đặc biệt: Tartaglia=Childe , raiden shogun = Baal ',
 	fields: [
                     {
                         name: '/build ',
