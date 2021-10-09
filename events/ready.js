51aa4d3a1cc89d064d68f9347ee67dbb06b60b86
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-09 22:03:23 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setPresence({ activities: [{ name: 'Genshin Impact',type: "STREAMING",url: "https://www.twitch.tv/tidvn" }], status: 'online' });
 		
 	},
 };
