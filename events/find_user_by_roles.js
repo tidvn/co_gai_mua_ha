6dd15787880bf0b5dbe487c613fa839c658df812
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-13 16:49:53 +0700

    Update find_user_by_roles.js

--- events/find_user_by_roles.js
+++ events/find_user_by_roles.js
@@ -1,3 +1,4 @@
+
 const {MessageEmbed} = require('discord.js');
 module.exports = {
 	name: 'message',
@@ -7,16 +8,23 @@ module.exports = {
             //if (message.author.id == client.user.id) return;
         
             if (message.content.startsWith("!roles")) {
-                const member = message.mentions.members.first() || message.member;
-            console.log(message.member)
-                const filteredRoles = member.roles.cache.filter(role => role.id != message.guild.id);
-                const listedRoles = filteredRoles.sort((a, b) => b.position - a.position).map(role => role.toString());
+                let roleName = message.content.split(" ").slice(1).join(" ");
+                members = message.guild.roles.cache.find(role => role.name === `${roleName}`).members.map(m=>m.user.tag);
+            console.log(members)
+
+               // const filteredRoles = member.roles.cache.filter(role => role.id != message.guild.id);
+              //  const listedRoles = filteredRoles.sort((a, b) => b.position - a.position).map(role => role.toString());
             
-                const embed = new MessageEmbed()
-                    .setTitle(member.user.username)
-                    .addField("Roles", listedRoles.join(","));
+                
+
+
+
+                
+                 const embed = new MessageEmbed()
+                     //.setTitle(member.user.username)
+                     .addField("Roles", members.join(","));
             
-                message.channel.send({embeds: [embed]});
+                 message.channel.send({embeds: [embed]});
             }
 		
 	},
