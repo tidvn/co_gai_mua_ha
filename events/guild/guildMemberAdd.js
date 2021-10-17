2be20fea75ecb3ba70084d535891238f95dc0b6c
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-18 00:42:33 +0700

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -61,8 +61,8 @@ module.exports = async (client, member) => {
     const welcome_eEmbed = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setTitle(`chào mừng đến với Genshin VN`)
-    .setDescription("dfgsfgfdg")
-    //.setDescription(`<:Kwc_horn:894052603179196476> Vui lòng đọc kỹ luật tại :\n <a:sparkle5:894052603707654184> ${member.guild.channels.cache.get('878862246414020628').toString()}\n <a:PinkStar2:894052602860408853> Và chọn role tại:\n<a:sparkle7:894052603141443685>${member.guild.channels.cache.get('879271959853154344').toString()}\n<:WannyHypeDONTSTEAL:894052603443429376> Chúc bạn có nhưng giây phút vui vẻ tại server của chúng tôi`)
+    //.setDescription("dfgsfgfdg")
+    .setDescription(`<:Kwc_horn:894052603179196476> Vui lòng đọc kỹ luật tại :\n <a:sparkle5:894052603707654184> ${member.guild.channels.cache.get('878862246414020628').toString()}\n <a:PinkStar2:894052602860408853> Và chọn role tại:\n<a:sparkle7:894052603141443685>${member.guild.channels.cache.get('879271959853154344').toString()}\n<:WannyHypeDONTSTEAL:894052603443429376> Chúc bạn có nhưng giây phút vui vẻ tại server của chúng tôi`)
     .setThumbnail('https://media2.giphy.com/media/HTVeYVXjLiunFlUOeu/giphy.gif?cid=ecf05e47q73m9htvgy80z6cwnpe5d3czv780mf5w3xns1nia&rid=giphy.gif&ct=g')
     .setImage("attachment://welcome-image.png");
 
