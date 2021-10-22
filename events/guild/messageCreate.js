0c0a75a6a924c0160ed5d7c227928b38184cb76a
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-22 14:40:54 +0700

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -6,6 +6,8 @@ const { onCoolDown, replacemsg } = require("../../handlers/functions");
 const Discord = require("discord.js");
 module.exports = async (client, message) => {
     if(!message.guild || !message.channel || message.author.bot) return;
+    require(`../../uti/anti-ad`)(message);
+    require(`../../uti/anti-bad-words.js`)(message);
     if(message.channel.partial) await message.channel.fetch();
     if(message.partial) await message.fetch();
     const prefix = process.env.PREFIX;
