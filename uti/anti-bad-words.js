615c1596207dbad24874d39458d3b95ee2670a8c
tidvn <74198018+tidvn@users.noreply.github.com>
2021-10-22 16:30:16 +0700

    Update anti-bad-words.js

--- uti/anti-bad-words.js
+++ uti/anti-bad-words.js
@@ -1,37 +1,13 @@
 const {badword} = require('../assets/badwords.json');
-module.exports= async(message)=>{
+module.exports= async (message)=>{
     
-    const isbad = async (bw, msg) => {
-        return await new Promise((resolve) => {
-         
-            for (var i in bw) {
-                
-              if (msg.toLowerCase().includes(badword[i].toLowerCase())) {
-                resolve(true)
-                return
-              }
-            }
-      
-            resolve(false)
-          })
-        
+    let foundInText = false;
+    for (var i in badword) {
+      if (message.content.toLowerCase().includes(badword[i].toLowerCase())) foundInText = true;
+    }
+    if (foundInText) {
+       
+        await  message.delete();// Deletes the message
+        await  message.channel.send(`bad`);
       }
-      const ibad = await isbad(badword, message.content);
-
-
-      if (ibad) {
-        message.delete();
-        message.channel.send(`tin nhắn có chứa từ bị cấm`);
-         // Deletes the message
-        
-        }
-
-    // let foundInText = false;
-    // for (var i in badword) {
-    //   if (message.content.toLowerCase().includes(badword[i].toLowerCase())) foundInText = true;
-    // }
-    // if (foundInText) {
-       
-       
-    //   }
     }
