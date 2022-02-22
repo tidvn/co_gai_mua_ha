afdd4e1c60b525332f5fedc3926e758768c1ceb9
Phùng Tiến Dũng <74198018+tidvn@users.noreply.github.com>
2022-02-22 14:53:24 +0700

    Update role.js

--- uti/role.js
+++ uti/role.js
@@ -31,40 +31,40 @@ module.exports= (interaction)=>{
 		}
 
 
-		if(interaction.customId=='ON') {
-			member.roles.remove(crole(`Newbie`)); 
-			member.roles.remove(crole(`Oldbie`));
-			switch (interaction.values[0]){
-				case 'Oldbie' :
-					member.roles.add(crole(`Oldbie`));
-				break;
-				case 'Newbie' :
-					member.roles.add(crole(`Newbie`));
-				break;
-			} 
-		}
+// 		if(interaction.customId=='ON') {
+// 			member.roles.remove(crole(`Newbie`)); 
+// 			member.roles.remove(crole(`Oldbie`));
+// 			switch (interaction.values[0]){
+// 				case 'Oldbie' :
+// 					member.roles.add(crole(`Oldbie`));
+// 				break;
+// 				case 'Newbie' :
+// 					member.roles.add(crole(`Newbie`));
+// 				break;
+// 			} 
+// 		}
 
-		if(interaction.customId=='Device') {
-			member.roles.remove(crole(`PC`)); 
-			member.roles.remove(crole(`Mobile`));
-			member.roles.remove(crole(`PS`));
-		if (interaction.values.includes(`Pc`)) member.roles.add(crole(`PC`));
-		if (interaction.values.includes(`Mobile`)) member.roles.add(crole(`Mobile`));
-		if (interaction.values.includes(`Ps`)) member.roles.add(crole(`PS`));
-		}
+// 		if(interaction.customId=='Device') {
+// 			member.roles.remove(crole(`PC`)); 
+// 			member.roles.remove(crole(`Mobile`));
+// 			member.roles.remove(crole(`PS`));
+// 		if (interaction.values.includes(`Pc`)) member.roles.add(crole(`PC`));
+// 		if (interaction.values.includes(`Mobile`)) member.roles.add(crole(`Mobile`));
+// 		if (interaction.values.includes(`Ps`)) member.roles.add(crole(`PS`));
+// 		}
 
-		if(interaction.customId=='Game') {
-			member.roles.remove(crole(`Genshin Impact`)); 
-			member.roles.remove(crole(`Valorant`));
-			member.roles.remove(crole(`League of Legends`));
-			member.roles.remove(crole(`Honkai Impact`));
-			member.roles.remove(crole(`Counter Strike`));
-		if (interaction.values.includes(`Genshin_Impact`)) member.roles.add(crole(`Genshin Impact`));
-		if (interaction.values.includes(`Honkai_Impact`)) member.roles.add(crole(`Honkai Impact`));
-		if (interaction.values.includes(`League_of_Legends`)) member.roles.add(crole(`League of Legends`));
-		if (interaction.values.includes(`Valorant`)) member.roles.add(crole(`Valorant`));
-		if (interaction.values.includes(`CSGO`)) member.roles.add(crole(`Counter Strike`));
-		}
+// 		if(interaction.customId=='Game') {
+// 			member.roles.remove(crole(`Genshin Impact`)); 
+// 			member.roles.remove(crole(`Valorant`));
+// 			member.roles.remove(crole(`League of Legends`));
+// 			member.roles.remove(crole(`Honkai Impact`));
+// 			member.roles.remove(crole(`Counter Strike`));
+// 		if (interaction.values.includes(`Genshin_Impact`)) member.roles.add(crole(`Genshin Impact`));
+// 		if (interaction.values.includes(`Honkai_Impact`)) member.roles.add(crole(`Honkai Impact`));
+// 		if (interaction.values.includes(`League_of_Legends`)) member.roles.add(crole(`League of Legends`));
+// 		if (interaction.values.includes(`Valorant`)) member.roles.add(crole(`Valorant`));
+// 		if (interaction.values.includes(`CSGO`)) member.roles.add(crole(`Counter Strike`));
+// 		}
 		
 		
 		interaction.deferUpdate();
